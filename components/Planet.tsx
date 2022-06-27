e4c988844c9285c94eeff7ac7131c9d8e960978a
rosarioCaramel <rosario@caramelpoint.com>
2022-06-27 11:36:09 -0300

    feat(added page planets and ssg): add react-query, SSG

---
+++ components/Planet.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+interface Planet {
+  id: any;
+  name: string;
+  population: string;
+  terrain: string
+}
+
+type Props = {
+  planet: any;
+};
+
+
+const Planet = ({ planet }: Props) => {
+  return (
+    <div>
+      <h3>{planet.name}</h3>
+      <p>Population - {planet.population}</p>
+      <p>Terrain - {planet.terrain} </p>
+      <hr />
+    </div>
+  );
+};
+export default Planet;
