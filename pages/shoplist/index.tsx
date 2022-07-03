f34903fe9af4d8dee5dfefc84a9921a9923c409e
RenzoLetona <renzo@caramelpoint.com>
2022-07-03 13:50:45 -0300

    feat(component): add thunk,actions,store from easy peasy

---
+++ pages/shoplist/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Foods from '../../components/ShopList/Foods'
+import AddFood from '../../components/ShopList/AddFood'
+import FoodData from './model'
+import { StoreProvider, createStore } from 'easy-peasy'
+
+const store = createStore(FoodData)
+
+export default function ShopList() {
+  return (
+    <StoreProvider store={store}>
+      <Foods />
+      <AddFood />
+    </StoreProvider>
+  );
+}
---
+++ pages/shoplist/model.tsx
@@ -0,0 +1,48 @@
+import { action, thunk, useStoreActions } from "easy-peasy"
+import { FoodModel, IFood } from "../../types/food-model.type";
+
+const FoodData: FoodModel = {
+  foods: [
+    { id: 21, name: 'Broccoli', price: 2, purchased: false },
+    { id: 22, name: 'Carrots', price: 4, purchased: true },
+    { id: 23, name: 'Apple', price: 8, purchased: false },
+  ],
+  //Actions
+  toggle: action((state, id) => {
+    state.foods.map((food: IFood) => {
+      if (food.id === Number(id)) {
+        food.purchased = !food.purchased;
+      }
+      return food;
+    })
+  }),
+  add: action((state, food) => {
+    //food.id = Math.floor(Math.random() * 20);
+    //food.name = "lala";
+    //food.price = 0;
+    //food.purchased = false;
+    state.foods = [...state.foods, food]
+  }),
+  setFood: action((state, comida: IFood[]) => {
+    state.foods = comida
+  }),
+
+  //Thunks
+  updateThunk: thunk(async (action) => {
+    const comidas = [
+      { id: 21, name: 'Broccoli', price: 2, purchased: false },
+      { id: 22, name: 'Carrots', price: 4, purchased: true },
+      { id: 23, name: 'Apple', price: 8, purchased: false },
+      { id: 23, name: 'Onion', price: 8, purchased: false },
+      { id: 23, name: 'Watermelon', price: 8, purchased: false },
+    ];
+
+    const response: IFood[] = await new Promise(resolve => setTimeout(() => {
+      resolve(comidas);
+    }, 300))
+
+    action.setFood(response)
+  }),
+}
+
+export default FoodData
