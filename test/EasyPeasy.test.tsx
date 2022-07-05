548160cd95cd0a09d02162b2c03e9e1cc547ce56
RenzoLetona <renzo@caramelpoint.com>
2022-07-05 17:39:53 -0300

    feat(component): create a test for each food action

--- test/EasyPeasy.test.tsx
+++ test/EasyPeasy.test.tsx
@@ -1,19 +1,36 @@
-/* eslint-disable no-undef*/
-
 import { createStore } from 'easy-peasy'
-import FoodItem from '../components/ShopList/FoodItem'
-import FoodData from '../models/model'
+import FoodData from '../models/food'
 
-test('is working', () => {
-  // arrange
-  const todo = { id: 30, name: 'hola', price: 31, purchased: false }
-  const store = createStore(FoodData)
+describe('Foods Actions', () => {
+  test('adding a food', () => {
+    const food = { id: 30, name: 'orange', price: 31, purchased: false }
+    const store = createStore(FoodData)
 
-  // act
-  store.getActions().add(todo)
+    store.getActions().add(food)
 
-  // assert
-  expect(store.getState().foods).toEqual([todo])
-})
+    expect(store.getState().foods).toEqual([food]) //this is supposed to fail because the initial state doesn't match
+  })
+
+  test('setting a new state of food', () => {
+    const foods = [
+      { id: 23, name: 'Onion', price: 8, purchased: false },
+      { id: 23, name: 'Watermelon', price: 8, purchased: false },
+    ]
+    const store = createStore(FoodData)
+
+    store.getActions().setFood(foods)
+
+    expect(store.getState().foods).toEqual(foods)
+  })
+
+  test('switching purchased attribute', () => {
+    const id = '21'
+    const store = createStore(FoodData)
+
+    store.getActions().toggle(id)
+
+    expect(store.getState().foods[0].purchased).toBeTruthy
+  })
+});
 
 export { }
